c55d29caf8edc6309ca418b58e8c04e0e0adbe5d
Sree Vignesh <sreevignesh256@gmail.com>
2024-09-22 22:07:41 +0530

    Changed to aligning items on baseline instead of centre.

--- src/plugins/widgets/time/Digital.tsx
+++ src/plugins/widgets/time/Digital.tsx
@@ -12,11 +12,23 @@ type Props = {
 };
 
 const Digital: FC<Props> = (props) => (
-  <div className="Time Digital" style={{display : 'flex',alignItems: 'center',justifyContent:'center',gap:'10px'}}>
-    <h1 >
+  <div
+    className="Time Digital"
+    style={{
+      display: "flex",
+      alignItems: "baseline",
+      // justifyContent: "center",
+      gap: "10px",
+    }}
+  >
+    <h1>
       <IntlTime {...props} />
     </h1>
-    {props.showSeconds &&  <h1 className="Seconds" ><IntlTimeSeconds {...props} /></h1>}
+    {props.showSeconds && (
+      <h1 className="Seconds">
+        <IntlTimeSeconds {...props} />
+      </h1>
+    )}
   </div>
 );
 
