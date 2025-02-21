6eb86a1b4d3eb28640bdbe7292b2b5163a31e7e1
BookCatKid <Simonhroff@icloud.com>
2025-02-21 13:25:39 -0800

    Fix Tests

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', {
+      tsconfig: 'tsconfig.test.json',
+    }],
+  },
+  // Add any other necessary Jest configurations
+};
